f7e9ead846d0de83405b84c518edd90d335fcaf4
quangkhai88 <canhcung88@gmail.com>
2022-02-07 01:34:28 +0100

    update

--- src/main/java/solid/di/before/User.java
+++ src/main/java/solid/di/before/User.java
@@ -12,6 +12,9 @@ public class User {
 	 *  we must use abstraction (abstract classes and interfaces) instead of concrete implementations.
 	 *  High-level modules should not depend on the low-level  module
 	 *  both should depend on the abstraction
+	 *
+	 * Here: user is depends on SmartPhone: low level and a detail implementation
+	 * => it should depend on high level and abstract Phone
 	 */
 	private SmartPhone smartPhone;
 	
